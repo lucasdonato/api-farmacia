4437eaf0d697a21ea0d0c1c523496c4d94aee300
Lucas Donato <lucaspolimig96@gmail.com>
2024-08-07 18:28:13 -0300

    alterando porta da aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import fastify from "fastify";
 
-export const server = fastify({ logger: false });
+export const server = fastify({ logger: true });
+const PORT = process.env.PORT || 3333;
 
 server
 	.listen({
 		host: "0.0.0.0",
-		port: 8080,
+		port: Number(PORT),
 	})
 	.then(() => {
 		console.log("🚀 HTTP Server Running!");
+	})
+	.catch((err) => {
+		console.error(err);
+		process.exit(1);
 	});
+
+export default server;
